3dbe29e35bbe6080b29ed6f5fe312dec362554d8
Sergey Ruppel <sergeyruppel@gmail.com>
2023-10-01 20:08:22 +0700

    Models added

---
+++ Alt Store/Alt Store/Models/App.swift
@@ -0,0 +1,17 @@
+//
+//  App.swift
+//  Alt Store
+//
+//  Created by Sergey Ruppel on 01.10.2023.
+//
+
+import UIKit
+
+struct App {
+    
+    let title: String
+    let price: Double
+    let icon: UIImage?
+    
+    let reviews = Array<Review>()
+}
---
+++ Alt Store/Alt Store/Models/Review.swift
@@ -0,0 +1,16 @@
+//
+//  Review.swift
+//  Alt Store
+//
+//  Created by Sergey Ruppel on 01.10.2023.
+//
+
+import Foundation
+
+struct Review {
+    
+    private let date = Date()
+    let text: String
+    let rating: Double
+    
+}
